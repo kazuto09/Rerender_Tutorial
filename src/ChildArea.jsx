edea2ea9bf13c187f1e90a9f23df257523c799b9
kazuto09 <55534698+kazuto09@users.noreply.github.com>
2021-12-30 14:48:26 +0900

    Initial commit

---
+++ src/ChildArea.jsx
@@ -0,0 +1,29 @@
+import { memo, React } from "react";
+
+const style = {
+  with: "100%",
+  height: "200px",
+  backgroundColor: "khaki"
+};
+
+export const ChildArea = memo((props) => {
+  const { open, onClickClose } = props;
+  console.log("チャイルドエリアがレンダリングがされた！");
+
+  const data = [...Array(2000).keys()];
+
+  data.forEach(() => {
+    console.log("...");
+  });
+
+  return (
+    <>
+      {open ? (
+        <div style={style}>
+          <p>子コンポーネント</p>
+          <button onClick={onClickClose}>閉じる</button>
+        </div>
+      ) : null}
+    </>
+  );
+});
